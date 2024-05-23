4f8cd63d18cadc1925a3607d7315f041cfb5e824
Guillaume <guillaume.marechal@epsi.fr>
2024-05-23 15:29:36 +0200

    Message front fait

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'messageUser.dart'; // Assurez-vous d'importer correctement le fichier messageUser.dart
 
 class MessagePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Contenu de la page Message ici'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Rechercher des messages',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [
+          MessageUser(), // Utilisez MessageUser() au lieu de MessagePage()
+          SizedBox(height: 4), // Espacement vertical entre les MessageUser
+          MessageUser(),
+          SizedBox(height: 4),
+          MessageUser(),
+          SizedBox(height: 4),
+          // Ajoutez autant de MessageUser que nécessaire ici
+        ],
+      ),
+    );
   }
 }
--- lib/models/messageUser.dart
+++ lib/models/messageUser.dart
@@ -1,8 +1,71 @@
 import 'package:flutter/material.dart';
 
-class MessagePage extends StatelessWidget {
+class MessageUser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Contenu de la page Message ici'));
+    return GestureDetector(
+      onTap: () {
+        // Action à effectuer lors du clic sur l'élément
+        print('MessageUser tapped!');
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
+        padding: EdgeInsets.all(10.0), // Ajouter un padding autour du contenu
+        constraints: BoxConstraints(maxHeight: 75.0), // Définir la hauteur maximale
+        decoration: BoxDecoration(
+          color: Colors.white, // Fond blanc
+          borderRadius: BorderRadius.horizontal(
+            left: Radius.circular(50.0),
+            right: Radius.circular(50.0),
+          ),
+          border: Border.all(
+            color: Colors.black, // Contour noir
+            width: 0.5,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25), // Ombre portée noire avec opacité
+              offset: Offset(2, 2), // Décalage vers le bas à droite
+              blurRadius: 4.0, // Flou
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipOval(
+              child: Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.blue, // Vous pouvez remplacer par l'image
+                child: Center(
+                  child: Icon(Icons.person, color: Colors.white, size: 30.0), // Ajuster la taille de l'icône
+                ), // Ajouter un peu d'espace à droite de l'image
+              ),
+            ),
+            SizedBox(width: 10.0), // Ajouter un espacement entre l'image et le texte
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Prénom Nom',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5.0), // Ajouter un espacement entre les deux textes
+                  Text('Dernier message : ...'),
+                ],
+              ),
+            ),
+            SizedBox(width: 10.0), // Ajouter un espacement entre le texte et la date
+            Text(
+              '23/05/2024',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
